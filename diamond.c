9a876c3ae120e355d24bd2c9b015b4f9c7ca582b
Uzair Ahmed <uzziahmed74@gmail.com>
2023-07-28 08:07:20 +0500

    Initial Commit

---
+++ diamond.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void){
+
+for(int i = 5;i>0;i--){
+
+    for(int j=1;j<i;j++)
+        {
+            printf(" ");
+        }
+    for(int j=i;j<=5;j++)
+        {
+            printf("*");
+        }
+
+    for(int j=i;j<=5;j++)
+        {
+            printf("*");
+        }
+    printf("\n");
+}
+for(int i = 1;i<=5;i++){
+
+    for(int j=1;j<i;j++)
+        {
+            printf(" ");
+        }
+    for(int j=i;j<=5;j++)
+        {
+            printf("*");
+        }
+
+    for(int j=i;j<=5;j++)
+        {
+            printf("*");
+        }
+    printf("\n");
+}
+/*
+for(int i = 1;i<=5;i++){
+    for(int j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+
+    for(int j=i;j<5;j++)
+        {
+            printf("  ");
+        }
+
+
+    for(int j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+    printf("\n");
+}*/
+getch();
+}
+
